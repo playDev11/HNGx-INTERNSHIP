0dae4184696186d8e13612fb2f648ed30d87a0a1
playDev11 <play.dev6@gmail.com>
2024-04-10 12:05:09 +0100

    feat: initial commit

---
+++ week one/main.js
@@ -0,0 +1,25 @@
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  function updateTime() {
+    const day = document.querySelector(".currentDay");
+    const Time = document.querySelector(".currentTime");
+  
+    const date = new Date().getDay();
+    const weekday = weekdays[date];
+  
+    const time = new Date().getTime();
+  
+    day.innerHTML = `${weekday}`;
+    Time.innerHTML = `${time}`;
+    setTimeout(() => updateTime(), 1000);
+  }
+  
+  updateTime();
